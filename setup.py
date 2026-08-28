b6bdc93d2bb4665db8e7fabe55968bd943d96ffd
dsoma5749-commits <dsoma5749@gmail.com>
2026-08-28 11:50:08 +0530

    Update author to Aquamarine Hoshino and rebuild distribution

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="aquamarine-nexus",
     version="1.0.0",
+    author="Aquamarine Hoshino",
     packages=find_packages(),
     include_package_data=True,
     entry_points={
