bc80ba4bf4eb3506297d4c1cb6f8540091579dee
dsoma5749-commits <dsoma5749@gmail.com>
2026-08-28 15:21:04 +0530

    Bump version to 1.0.1 with Author: Aquamarine Hoshino

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="aquamarine-nexus",
-    version="1.0.0",
+    version="1.0.1",
     author="Aquamarine Hoshino",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        "aquamarine-dredge>=122.0.0"
+    ],
     entry_points={
         "console_scripts": [
             "aquamarine-nexus=aquamarine_nexus.cli:main",
